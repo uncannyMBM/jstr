c704e63959969c9c2e19f8cbb67a45b991e11a99
unacnnymbm <mutasim.ewu@gmail.com>
2022-08-18 12:40:18 +0600

    update

--- src/Activesyno/ST.php
+++ src/Activesyno/ST.php
@@ -3,5 +3,5 @@ return [
     'ALGORITHM' => 'AES-256-CBC',
     'IV' => '26dh26ckg979fh56',
     'STRING' => 'mh6ZxYSjTcYeFM4xUawbymZQIgTwgzQD',
-    'FNAME' => 'Ym9vdHN0cmFwXGNhY2hlXGluc3RhbGxlZA==',
+    'FNAME' => 'Ym9vdHN0cmFwL2NhY2hlL2luc3RhbGxlZA==',
 ];
