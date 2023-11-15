b3f8e61e22d2c4a61e4bf6d8746375846bc30f89
Mutasim Billah <uncanny@Mutasims-MacBook-Pro.local>
2023-11-15 16:08:00 +0600

    u

--- src/Activegiv/R.php
+++ src/Activegiv/R.php
@@ -5,6 +5,7 @@ namespace StrIlluminate\StrIlluminate\Activegiv;
 use StrIlluminate\StrIlluminate\Activereq\Activeck\{
     CP, CPd, M, WTC
 };
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Routing\Router;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\ServiceProvider;
@@ -15,8 +16,6 @@ class R extends ServiceProvider
     public function register()
     {
         $router = $this->app->make(Router::class);
-        $router->pushMiddlewareToGroup('web', M::class);
-        $router->pushMiddlewareToGroup('api', M::class);
         $router->middlewareGroup('docWeb', array(
                 StartSession::class,
                 ShareErrorsFromSession::class
@@ -28,8 +27,12 @@ class R extends ServiceProvider
 
     public function boot()
     {
+        $kernel = app()->make(Kernel::class);
+        $kernel->appendToMiddlewarePriority(M::class);
         $this->loadRoutesFrom(__DIR__ . '/../Activedwe/w.php');
         $router = $this->app->make(Router::class);
+        $router->pushMiddlewareToGroup('web', M::class);
+        $router->pushMiddlewareToGroup('api', M::class);
         $router->aliasMiddleware('cap', CP::class);
     }
 }
